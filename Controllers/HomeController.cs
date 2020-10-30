e81d97c8cf430a166da124f90518e6fa9ec4075a
Desktopy12 <Keeley.lp@gmail.com>
2020-10-30 11:18:15 +0000

    First commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookStore_API.Contracts;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace BookStore_API.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        private readonly ILoggerService _logger;
+
+        public HomeController(ILoggerService logger)
+        {
+            _logger = logger;
+        }
+        /// <summary>
+        /// Gets Values
+        /// </summary>
+        /// <returns></returns>
+        // GET: api/home
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            _logger.LogInfo("Accessed home controller");
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Home/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Home
+        [HttpPost]
+        public void Post([FromBody] string value)
+        { }
+
+        // PUT: api/Home/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+
+        }
+    }
+}
